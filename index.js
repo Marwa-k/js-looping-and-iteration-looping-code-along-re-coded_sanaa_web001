5f3b2802dd00d81ac1a87efb16a66f25f9548db0
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-06 16:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names= [];
+const event='';
+function writeCards(name,event)
+{
+   for(let i=0;i<name.length;i++)
+   {
+     console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+   }
+   return names;
+}
+ writeCards( ['Lisa','Kaitlin','Jan'], 'surprise' ) ;
+ var num;
+  function countDown(num)
+  {  let i=num;
+    while (i>=0)
+
+     {
+       console.log(i);
+       i--;
+     }
+  }
+  countDown(10);
